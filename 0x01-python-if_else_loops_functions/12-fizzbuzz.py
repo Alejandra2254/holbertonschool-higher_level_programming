1eda3ebfada7bbfc5622911de8f9c84de371d83a
Alejandra2254 <alejandra.hmsc@gmail.com>
2020-12-08 15:38:37 -0500

    fizz buzz file

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for n in range (1, 100):
+        if n % 5 == 0 or n % 3 == 0:
+            if n % 3 == 0:
+                print("Fizz", end="")
+            if n % 5 == 0:
+                print("Buzz", end="")
+        else:
+            print("{:d}".format(n), end="")
+        if n < 100:
+            print(" ", end="")
