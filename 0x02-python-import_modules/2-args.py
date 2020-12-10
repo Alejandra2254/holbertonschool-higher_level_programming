d558d674a4a80b4f1b47901c399a663666a2dfce
Alejandra2254 <alejandra.hmsc@gmail.com>
2020-12-10 11:28:23 -0500

    Import sys module to use argv

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv
+    arg = len(argv)
+    n = 1
+    num = 0
+    if arg == 2:
+        print("1 argument.")
+    else:
+        print("{:d} arguments.".format(arg - 1))
+    while arg != n:
+        a = argv[n]
+        n += 1
+        print("{:d}: {:s}".format(n - 1, str(argv[n - 1])))
