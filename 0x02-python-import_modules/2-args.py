691f02c8104537f85c941682b4c6f463974c1bc9
Alejandra2254 <70872773+Alejandra2254@users.noreply.github.com>
2020-12-10 12:05:24 -0500

    Update 2-args.py

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,10 +4,12 @@ if __name__ == "__main__":
     arg = len(argv)
     n = 1
     num = 0
-    if arg == 2:
-        print("1 argument.")
-    else:
+    if arg == 1:
         print("{:d} arguments.".format(arg - 1))
+    elif arg == 2:
+        print("1 argument:")
+    else:
+        print("{:d} arguments:".format(arg - 1))
     while arg != n:
         n += 1
         print("{:d}: {:s}".format(n - 1, str(argv[n - 1])))
