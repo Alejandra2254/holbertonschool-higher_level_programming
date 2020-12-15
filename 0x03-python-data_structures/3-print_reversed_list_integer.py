4593ba1b00da74802edcab8ee8438d29918b8f7f
Alejandra2254 <alejandra.hmsc@gmail.com>
2020-12-15 10:32:15 -0500

    Added function that print the elements on reverse

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_reversed_list_integer(my_list=[]):
+    i = 0
+    n = len(my_list)
+    my_list.reverse()
+    while i < n:
+        print("{:d}".format(my_list[i]))
+        i +=1
