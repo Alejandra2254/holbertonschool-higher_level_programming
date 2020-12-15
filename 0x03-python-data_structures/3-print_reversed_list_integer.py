0a42c667a6f7bf388ffd0bca422de0c79fbb5a3f
Alejandra2254 <alejandra.hmsc@gmail.com>
2020-12-15 12:21:17 -0500

    Fixed format errors on task 3

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def print_reversed_list_integer(my_list=[]):
-    i = 0
-    n = len(my_list)
-    my_list.reverse()
-    while i < n:
-        print("{:d}".format(my_list[i]))
-        i += 1
+    if my_list:
+        i = 0
+        n = len(my_list)
+        my_list.reverse()
+        while i < n:
+            print("{:d}".format(my_list[i]))
+            i += 1
