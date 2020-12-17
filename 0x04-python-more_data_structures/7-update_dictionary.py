46521b91f9f477a518ecd7f2b07ee832a62a25c4
Alejandra2254 <70872773+Alejandra2254@users.noreply.github.com>
2020-12-17 12:06:47 -0500

    Update 7-update_dictionary.py

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    n = set(a_dictionary)
-    for i in n:
-        if i == key:
-            a_dictionary[i] = value
-        else:
-            a_dictionary[key] = value
+    a_dictionary[key] = value
     return a_dictionary
