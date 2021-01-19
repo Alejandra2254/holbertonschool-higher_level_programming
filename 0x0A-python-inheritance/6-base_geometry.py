64e6ffb6e9dda0676e515eacef4db4cf8a1e9715
Alejandra2254 <alejandra.hmsc@gmail.com>
2021-01-19 12:09:56 -0500

    Creating object class with a instance method

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+class BaseGeometry:
+    """class BaseGeometry based on task 5 but with
+    other instances"""
+    def area(self):
+        """
+        public instance method: def area(self): that raises
+        an Exception with the message area() is not implemented
+        """
+        raise Exception("area() is not implemented")
