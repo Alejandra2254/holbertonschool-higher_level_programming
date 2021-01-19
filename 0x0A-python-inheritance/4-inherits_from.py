c2564c0d68a789e9fe36ace10d2f0c7cec28ee6f
Alejandra2254 <alejandra.hmsc@gmail.com>
2021-01-19 11:54:47 -0500

    task 4, using function issubclass

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def inherits_from(obj, a_class):
+    """ function that returns True if the object is an
+    instance of a class that inherited """
+    if issubclass(type(obj), a_class) and type(obj) is not a_class:
+        return True
+    else:
+        return False
