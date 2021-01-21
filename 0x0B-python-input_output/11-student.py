302bb8815db91ccc9691ecc6d8b3b9c0e3b87136
Alejandra2254 <70872773+Alejandra2254@users.noreply.github.com>
2021-01-20 21:28:57 -0500

    Update 11-student.py

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -3,34 +3,34 @@
 
 
 class Student():
-        """Student class"""
+    """Student class"""
 
-            def __init__(self, first_name, last_name, age):
-                        """Student constructor"""
-                                self.first_name = first_name
-                                        self.last_name = last_name
-                                                self.age = age
+    def __init__(self, first_name, last_name, age):
+        """Student constructor"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
 
-                                                    def to_json(self, attrs=None):
-                                                                '''Returns dictionary description of json object filtered'''
-                                                                        flag = False
-                                                                                if (type(attrs) is list):
-                                                                                                flag = True
-                                                                                                            for attr in attrs:
-                                                                                                                                if type(attr) is not str:
-                                                                                                                                                        flag = False
-                                                                                                                                                                if flag is True:
-                                                                                                                                                                                my_dict = {}
-                                                                                                                                                                                            for key, value in self.__dict__.items():
-                                                                                                                                                                                                                if key in attrs:
-                                                                                                                                                                                                                                        my_dict[key] = value
-                                                                                                                                                                                                                                                else:
-                                                                                                                                                                                                                                                                my_dict = self.__dict__.copy()
+    def to_json(self, attrs=None):
+        '''Returns dictionary description of json object filtered'''
+        flag = False
+        if (type(attrs) is list):
+            flag = True
+            for attr in attrs:
+                if type(attr) is not str:
+                    flag = False
+        if flag is True:
+            my_dict = {}
+            for key, value in self.__dict__.items():
+                if key in attrs:
+                    my_dict[key] = value
+        else:
+            my_dict = self.__dict__.copy()
 
-                                                                                                                                                                                                                                                                        return my_dict
+        return my_dict
 
-                                                                                                                                                                                                                                                                        def reload_from_json(self, json):
-                                                                                                                                                                                                                                                                                    """Updates attributes with json"""
+    def reload_from_json(self, json):
+        """Updates attributes with json"""
 
-                                                                                                                                                                                                                                                                                            for key, value in json.items():
-                                                                                                                                                                                                                                                                                                            self.__dict__[key] = value''''
+        for key, value in json.items():
+            self.__dict__[key] = value
