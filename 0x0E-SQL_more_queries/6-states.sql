dbe05cb11f95fd37c24459e7570172f434b0c5ee
Alejandra2254 <alejandra.hmsc@gmail.com>
2021-03-02 22:15:34 -0500

    tasks 4 and 5

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,7 @@
+-- creates the database hbtn_0d_usa and the table states
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+-- create table states
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.states (
+       id INT UNIQUE AUTO_INCREMENT NOT NULL PRIMARY KEY,
+       name VARCHAR(256) NOT NULL
+);
