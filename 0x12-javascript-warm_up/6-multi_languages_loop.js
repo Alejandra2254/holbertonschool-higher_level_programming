c8fc75a718da8c618e0cc40f3132bad839ac06aa
Alejandra2254 <alejandra.hmsc@gmail.com>
2021-03-22 18:35:27 -0500

    Using loop task 6

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,8 @@
+#!/usr/bin/node
+// Write a script that prints 3 lines
+const arr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+let i = 0;
+while (arr[i]) {
+  console.log(arr[i]);
+  i++;
+}
