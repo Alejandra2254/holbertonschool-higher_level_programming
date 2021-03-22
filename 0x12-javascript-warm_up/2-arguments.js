f51af815b8596a563d9f84f689b3e5ea1ceff702
Alejandra2254 <alejandra.hmsc@gmail.com>
2021-03-22 16:11:06 -0500

    Task 2, handling arguments

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -2,9 +2,9 @@
 // Write a script that prints 3 lines
 const process = require('process');
 let args = process.argv;
-if (args.length === 2)
-    console.log('No argument');
 if (args.length === 3)
     console.log('Argument found');
-if (args.length > 3)
+else if (args.length > 3)
     console.log('Arguments found');
+else
+    console.log('No argument')
