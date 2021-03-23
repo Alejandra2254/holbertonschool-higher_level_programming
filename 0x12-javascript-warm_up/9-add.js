c15fa4d0d7cda365a0c30c6506a50d6e0b0bf2b6
Alejandra2254 <alejandra.hmsc@gmail.com>
2021-03-22 19:08:02 -0500

    Adding two numbers task 9

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,6 @@
+#!/usr/bin/node
+// Write a script that prints a square
+const count = process.argv;
+const number1 = parseInt(count[2]);
+const number2 = parseInt(count[3]);
+console.log(number1 + number2);
