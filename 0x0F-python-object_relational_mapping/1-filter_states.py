f133ed6a052663237883ac07fa0d8bc846ddfabc
Alejandra2254 <70872773+Alejandra2254@users.noreply.github.com>
2021-03-27 12:12:43 -0500

    Update 1-filter_states.py

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,16 +1,21 @@
 #!/usr/bin/python3
-import MySQLdb
-import sys
-if __name__ == "__main__":
+'''Query with MySQLdb'''
 
-    db = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute("SELECT * FROM states ORDER BY id")
-    my_states = c.fetchall()
-    for state in my_states:
-        if state[1][0] == 'N':
-            print(state)
-    c.close()
+
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
+
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3], charset="utf8")
+    cur = db.cursor()
+    cur.execute("\
+    SELECT * FROM states WHERE states.name REGEXP '^N' \
+    ORDER BY states.id ASC;")
+    query_rows = cur.fetchall()
+    for row in query_rows:
+        if row[1][0] == 'N':
+            print(row)
+    cur.close()
     db.close()
     
