3ffb57e2f35affa8b1c883e4e75372ae366cb0e5
Alejandra2254 <2254@holbertonschool.com>
2021-03-28 16:10:05 -0500

    TASK 8

--- 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
+++ 0x0F-python-object_relational_mapping/8-model_state_fetch_first.py
@@ -16,3 +16,4 @@ if __name__ == "__main__":
     session = Session()
     for results in session.query(State).order_by(State.id):
         print("{}: {}".format(results.id, results.name))
+        break
