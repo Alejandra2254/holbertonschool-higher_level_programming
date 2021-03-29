d4c6807124faaac23de6f2cd14edd33e53c187d7
Alejandra2254 <70872773+Alejandra2254@users.noreply.github.com>
2021-03-28 22:12:12 -0500

    Update relationship_state.py

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -1,17 +1,17 @@
 #!/usr/bin/python3
-'''first ORM model - state'''
-from sqlalchemy.ext.declarative import declarative_base
+"""
+Define State Class and an instance Base
+"""
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship
-
-
-Base = declarative_base()
+from relationship_city import Base, City
 
 
 class State(Base):
-    '''class definition of a State'''
+    """Represent a state"""
 
-    __tablename__ = 'states'
-    id = Column(Integer, primary_key=True, nullable=False, unique=True)
+    __tablename__ = "states"
+    id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
     cities = relationship("City", backref="state", cascade="all, delete")
