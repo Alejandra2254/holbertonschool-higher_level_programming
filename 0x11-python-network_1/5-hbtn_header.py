1670a7dcc1ac4413f9637609def3923d286d0417
Alejandra2254 <2254@holbertonschool.com>
2021-04-09 13:17:28 -0500

    Fixing with use arguments

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 '''fetches with the module requests'''
 import requests
+import sys
 
 
 if __name__ == "__main__":
-    r = requests.get('https://intranet.hbtn.io/status')
+    r = requests.get(sys.argv[1])
     header = {}
     header = r.headers
     print(header['X-request-ID'])
