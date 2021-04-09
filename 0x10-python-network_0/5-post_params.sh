57b1a68a031f2e3970db64d2d5c96ee440e567e9
Alejandra2254 <70872773+Alejandra2254@users.noreply.github.com>
2021-04-08 20:45:36 -0500

    Update 5-post_params.sh

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #  takes in a URL as an argument, sends a GET request to the URL, 
-curl "$1" -X POST --data "email=hr@holbertonschool.com&subject=I will always be here for PLD"
+curl "$1" -sX POST --data "email=hr@holbertonschool.com&subject=I will always be here for PLD"
