b2c92b0fe5385a42b24d3027833878defab54ebe
Alejandra2254 <70872773+Alejandra2254@users.noreply.github.com>
2021-04-09 13:04:48 -0500

    Adding r.text

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -5,6 +5,7 @@ import requests
 
 if __name__ == "__main__":
     req = requests.get('https://intranet.hbtn.io/status')
+    req = r.txt
     print('Body response:')
     print('\t- type: {}'.format(type(req)))
     print('\t- content: {}'.format(req))
