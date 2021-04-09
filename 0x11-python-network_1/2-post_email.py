6670508fdb68436681ca25ebe19e93645100751e
Alejandra2254 <2254@holbertonschool.com>
2021-04-09 12:34:05 -0500

    Fixing task2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
-''' takes in a URL and an email, sends a POST request
-    to the passed URL with the email as a parameter, and
-    displays the body of the response'''
+''' takes in a URL and an email, sends a POST request'''
 import urllib.request
+import urllib.parse
 import sys
 
 url = sys.argv[1]
