1d2ffb1705f43c7ffca836c4f8a77faed52a6f48
Alejandra2254 <2254@holbertonschool.com>
2021-04-09 12:35:54 -0500

    Fixing task2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 ''' takes in a URL and an email, sends a POST request'''
-import urllib.request
-import urllib.parse
+from urllib import request, parse
 import sys
 
-url = sys.argv[1]
-value = {'email': sys.argv[2]}
 
-data = urllib.parse.urlencode(value)
-data = data.encode('ascii')  # data should be bytes
-req = urllib.request.Request(url, data)
-with urllib.request.urlopen(req) as response:
-    the_page = response.read()
-    print(the_page.decode('utf-8'))
+if __name__ == "__main__":
+    url = sys.argv[1]
+    value = {'email': sys.argv[2]}
+
+    data = urllib.parse.urlencode(value)
+    data = data.encode('ascii')  # data should be bytes
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
+        the_page = response.read()
+        print(the_page.decode('utf-8'))
