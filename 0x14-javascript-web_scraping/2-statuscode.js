c2594f3ed23793a1ae541012a645810ab7a33868
Alejandra2254 <2254@holbertonschool.com>
2021-05-04 15:15:05 -0500

    Adding task2

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 // function to read a file passing as argumment
-var request = require('request');
+const request = require('request');
 const url_r = process.argv[2];
 
 request.get(url_r).on('response', function (response) {
