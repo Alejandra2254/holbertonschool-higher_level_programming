da68c5e66dc3b67d2ea0f991fa0b741f9a4c5706
Alejandra2254 <2254@holbertonschool.com>
2021-05-04 16:49:58 -0500

    Fixing task 6 by semistandard

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -8,13 +8,12 @@ request(url, function (error, response, body) {
     const stats = {};
     for (const x in results) {
       const data = results[x];
-      if (data.completed === true) {
+      if (data.completed === true) { // separes dicts where completed is true
         const id = data.userId;
-        if (id in stats) {
+        if (id in stats) { // check if id exist on the dic, if exist add 1
           stats[id] += 1;
-        }
-        else {
-          stats[id] = 1;
+        } else {
+          stats[id] = 1; // if no exist, created with value 1
         }
       }
     }
