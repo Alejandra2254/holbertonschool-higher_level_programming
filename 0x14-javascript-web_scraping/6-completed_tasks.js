cc21097ccd6d261ad2b9c4e8cb1d29bd5a0a6773
Alejandra2254 <2254@holbertonschool.com>
2021-05-04 16:42:56 -0500

    Adding task 6

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+// function to read a file passing as argumment
+const url = process.argv[2];
+const request = require('request');
+request(url, function (error, response, body) {
+  if (!error && response.statusCode === 200) {
+    const results = JSON.parse(body);
+    const stats = {};
+    for (const x in results) {
+      const data = results[x];
+      if (data.completed === true) {
+        const id = data.userId;
+        if (id in stats) {
+          stats[id] += 1;
+        }
+        else {
+          stats[id] = 1;
+        }
+      }
+    }
+    console.log(stats);
+  }
+});
